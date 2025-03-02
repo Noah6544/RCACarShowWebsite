aa804a7826a67dd1287407f2c65234b2ebd4c883
Noah Buchanan <buchanannoah100@gmail.com>
2025-03-02 00:00:38 -0500

    fixed it up for mr smith, made website snappier and better it was annoying me so bad. also i added my website update .com

--- static/javascript/loader.js
+++ static/javascript/loader.js
@@ -1,17 +1,40 @@
 document.addEventListener('DOMContentLoaded',() => {
+    // window.scrollTo(0,0);
     const loader = document.getElementById('loader');
     
     window.addEventListener('load', function(){
-        loader.innerHTML = "Done.";
+      
+        loader.style.cssText = "transition: all .2s ease; opacity: 0"
+        setTimeout(() => {
+            loader.innerHTML = "Enjoy The Show!";
+            loader.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+        }, 250); // Adjust the timing as needed
+        loader.classList.add('loaderLoaded');
 
-        loader.style.cssText = "transition: all 2s ease; opacity: 0; z-index: -9999";
+
+        // setTimeout(() => {
+        //     loader.style.cssText = "transition: all 2s ease; opacity: 0; z-index: -9999";
+        // }, 200); // Adjust the timing as needed
+
+
         const galleryThirdDivs = document.getElementsByClassName('galleryThird');
+        const leftGallery = document.getElementById('leftGallery');
+        const middleGallery = document.getElementById('middleGallery');
+        const rightGallery = document.getElementById('rightGallery');
+
+        console.log(galleryThirdDivs)
         const textContainer = document.getElementById('indexPage')
         const navbar = document.getElementById('navbar')
-        for( i =0; i < galleryThirdDivs.length; i++){
-            element = galleryThirdDivs[i];
-            element.classList.toggle('galleryLoaded');
-        }
+        leftGallery.classList.toggle('galleryLoaded');
+        middleGallery.classList.toggle('galleryLoaded');
+        rightGallery.classList.toggle('galleryLoaded');
+        // Array.from(galleryThirdDivs).forEach(element => {
+        //     element.classList.toggle('galleryLoaded');
+        // });
+        // for( i =0; i < galleryThirdDivs.length; i++){
+        //     element = galleryThirdDivs[i];
+        //     element.classList.toggle('galleryLoaded');
+        // }
         navbar.classList.toggle('navbarLoaded');
         textContainer.classList.toggle('textLoaded');
 
--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,73 +1,230 @@
+// 
 document.addEventListener('DOMContentLoaded',() => {
     
 //getting all text containers
 
-const indexPage = document.getElementById('indexPage')
-const detailsPage = document.getElementById('detailsPage')
-const registerPage = document.getElementById('registerPage')
+const indexPage = document.getElementById('indexPage');
+const detailsPage = document.getElementById('detailsPage');
+const registerPage = document.getElementById('registerPage');
 //getting all navbar items 
 
-const aboutButton = document.getElementById('aboutNavbar')
-const detailsButton = document.getElementById('detailsNavbar')
-const registerButton = document.getElementById('registerNavbar')
-const homeButton = document.getElementById('homeNavbar')
+const aboutButton = document.getElementById('aboutNavbar');
+const detailsButton = document.getElementById('detailsNavbar');
+const vehicleRegisterButton = document.getElementById('vehicleRegisterNavbar');
+const vendorRegisterButton = document.getElementById('vendorRegisterNavbar');
+const sponsorRegisterButton = document.getElementById('sponsorRegisterNavbar');
 
-aboutButton.addEventListener('click', function(){
-    let textContainer =  indexPage.querySelector('.textContainer')
-    textContainer.style.cssText = "transition: all .25s ease; opacity: 0"
-    let newText = aboutButton.querySelector('#aboutPageInformation').querySelector('.textContainer');
-    setTimeout(() => {
-        textContainer.innerHTML = newText.innerHTML;
-        textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+const homeButton = document.getElementById('homeNavbar');
+const mediaButton = document.getElementById('mediaNavbar');
 
-    }, 250); // Adjust the timing as needed
+const registerButton = document.getElementById("registerNavbar");
+const registerDropdown = document.getElementById("registerDropdown");
 
-});
+// window.addEventListener('resize', function(){ this idea is redacted because you'd need to remove the event listeners when entering a new side of the mobile true/false if statement 
 
+if(Number(window.outerWidth) > 699){
+    isMobile = false;
+}
 
-detailsButton.addEventListener('click', function(){
-    let textContainer =  indexPage.querySelector('.textContainer')
-    textContainer.style.cssText = "transition: all .25s ease; opacity: 0"
-    let newText = detailsButton.querySelector('#detailsPageInformation').querySelector('.textContainer');
-
-    setTimeout(() => {
-        textContainer.innerHTML = newText.innerHTML;
-        textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
-
-
-    }, 250); // Adjust the timing as needed
+else{
+    isMobile = true;
+}
 
 
 
-});
+
+    
+//I'm so sorry if someone is trying to edit this in the future,
+//this was my first scuffed approach that I didn't bother automating. good luck 
+///- Noah Buchanan March 2024
 
 
-registerButton.addEventListener('click', function(){
-    let textContainer =  indexPage.querySelector('.textContainer')
-    textContainer.style.cssText = "transition: all .25s ease; opacity: 0"
-    let newText = registerButton.querySelector('#registerPageInformation').querySelector('.textContainer');
-    setTimeout(() => {
-        textContainer.innerHTML = newText.innerHTML;
-        textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+//we have to seperate the events into mobile and compute cuz click events don't work.
+//DESKTOP CLICK EVENTS
+
+if(isMobile === false){
+    vehicleRegisterButton.addEventListener('click', function(){
+        let textContainer = indexPage.querySelector('.textContainer')
+        textContainer.style.cssText = "transition: all .1s ease; opacity: 0"
+        let newText = vehicleRegisterButton.querySelector('#vehicleRegisterPageInformation').querySelector('.textContainer');
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+        }, 250); // Adjust the timing as needed
+    });
+    
+    vendorRegisterButton.addEventListener('click', function(){
+        let textContainer =  indexPage.querySelector('.textContainer')
+        textContainer.style.cssText = "transition: all .1s ease; opacity: 0"
+        let newText = vendorRegisterButton.querySelector('#vendorRegisterPageInformation').querySelector('.textContainer');
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+        }, 250); // Adjust the timing as needed
+    });
+
+    sponsorRegisterButton.addEventListener('click', function(){
+        let textContainer =  indexPage.querySelector('.textContainer')
+        textContainer.style.cssText = "transition: all .1s ease; opacity: 0"
+        let newText = sponsorRegisterButton.querySelector('#sponsorRegisterPageInformation').querySelector('.textContainer');
+        console.log(newText);
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+        }, 250); // Adjust the timing as needed
+    });    
+
+    detailsButton.addEventListener('click', function(){
+        let textContainer =  indexPage.querySelector('.textContainer')
+        textContainer.style.cssText = "transition: all .25s ease; opacity: 0"
+        let newText = detailsButton.querySelector('#detailsPageInformation').querySelector('.textContainer');
+    
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+    
+    
+        }, 250); // Adjust the timing as needed
+    
+    
+    
+    });
+    
+    mediaButton.addEventListener('click', function(){
+        let textContainer =  indexPage.querySelector('.textContainer')
+        textContainer.style.cssText = "transition: all .25s ease; opacity: 0"
+        let newText = mediaButton.querySelector('#mediaPageInformation').querySelector('.textContainer');
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+    
+        }, 250); // Adjust the timing as needed
+    
+    });   
+    aboutButton.addEventListener('click', function(){
+        
+        let textContainer =  indexPage.querySelector('.textContainer')
+        textContainer.style.cssText = "transition: all .25s ease; opacity: 0"
+        let newText = aboutButton.querySelector('#aboutPageInformation').querySelector('.textContainer');
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+
+        }, 250); // Adjust the timing as needed
+
+    });
+    homeButton.addEventListener('click', function(){
+        let textContainer =  indexPage.querySelector('.textContainer')
+        textContainer.style.cssText = "transition: all .25s ease; opacity: 0"
+    
+        let newText = homeButton.querySelector('#indexPageInformation').querySelector('.textContainer');
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+    
+    
+        }, 250); // Adjust the timing as needed
+    });
+}
+
+else{
+//MOBILE TOUCH EVENTS
+    registerButton.addEventListener('touchend', function(){
+        registerDropdown.classList.toggle("dropdownShow"); //this will show the dropdown.
+
+    });
+    vehicleRegisterButton.addEventListener('touchend', function(){
+        let textContainer = indexPage.querySelector('.textContainer');
+        textContainer.style.cssText = "transition: all .25s ease; opacity: 0;";
+        let newText = vehicleRegisterButton.querySelector('#vehicleRegisterPageInformation').querySelector('.textContainer');
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+        }, 250); // Adjust the timing as needed
+        
+    });
+
+    vendorRegisterButton.addEventListener('touchend', function(){
+        let textContainer = indexPage.querySelector('.textContainer')
+        textContainer.style.cssText = "transition: all .25s ease; opacity: 0"
+        let newText = vendorRegisterButton.querySelector('#vendorRegisterPageInformation').querySelector('.textContainer');
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+        }, 250); // Adjust the timing as needed
+    });
+    
+    sponsorRegisterButton.addEventListener('touchend', function(){
+        let textContainer =  indexPage.querySelector('.textContainer')
+        textContainer.style.cssText = "transition: all .1s ease; opacity: 0"
+        let newText = sponsorRegisterButton.querySelector('#sponsorRegisterPageInformation').querySelector('.textContainer');
+        console.log(newText);
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+        }, 250); // Adjust the timing as needed
+    });  
+
+    detailsButton.addEventListener('touchend', function(){
+
+        let textContainer =  indexPage.querySelector('.textContainer')
+        textContainer.style.cssText = "transition: all .25s ease; opacity: 0";
+        let newText = detailsButton.querySelector('#detailsPageInformation').querySelector('.textContainer');
+
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
 
 
-    }, 250); // Adjust the timing as needed
+        }, 250); // Adjust the timing as needed
 
 
-});
 
-homeButton.addEventListener('click', function(){
-    let textContainer =  indexPage.querySelector('.textContainer')
-    textContainer.style.cssText = "transition: all .25s ease; opacity: 0"
+    });
 
-    let newText = homeButton.querySelector('#indexPageInformation').querySelector('.textContainer');
-    setTimeout(() => {
-        textContainer.innerHTML = newText.innerHTML;
-        textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+    mediaButton.addEventListener('touchend', function(){
+        let textContainer =  indexPage.querySelector('.textContainer');
+        textContainer.style.cssText = "transition: all .25s ease; opacity: 0;";
+        let newText = mediaButton.querySelector('#mediaPageInformation').querySelector('.textContainer');
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+
+        }, 250); // Adjust the timing as needed
+
+    });
+
+    aboutButton.addEventListener('touchend', function(){
+        
+        let textContainer =  indexPage.querySelector('.textContainer');
+        textContainer.style.cssText = "transition: all .25s ease; opacity: 0;";
+        let newText = aboutButton.querySelector('#aboutPageInformation').querySelector('.textContainer');
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
+
+        }, 250); // Adjust the timing as needed
+
+    });
+    homeButton.addEventListener('touchend', function(){
+        let textContainer =  indexPage.querySelector('.textContainer');
+        textContainer.style.cssText = "transition: all .25s ease; opacity: 0"
+
+        let newText = homeButton.querySelector('#indexPageInformation').querySelector('.textContainer');
+        setTimeout(() => {
+            textContainer.innerHTML = newText.innerHTML;
+            textContainer.style.opacity = 1; //DO NOT use style.csstext =, it ruins the transition or something.
 
 
-    }, 250); // Adjust the timing as needed
-});
+        }, 250); // Adjust the timing as needed
+    });
+
+}
+
+
+    history.scrollRestoration = "manual";
+    window.scrollTo(0,0); //these 2 lines ensure that the document goes back to the top left of the document- I'd been having issues recently with keeping it in line.
+
 
 
 });
